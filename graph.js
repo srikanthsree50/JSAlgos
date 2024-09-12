5d71249cc80e018d7756ff36179a34b21ff95765
srikanthsree50 <srikanthsree50@gmail.com>
2024-09-12 21:51:06 +0530

    graphs commit

---
+++ graph.js
@@ -0,0 +1,36 @@
+class Graph{
+    constructor(){
+        this.adjacencyList = {}
+    }
+    addVertex(vertex) {
+        this.adjacencyList[vertex] = [];
+    }
+    addEdge(vertex1,vertex2){
+this.adjacencyList[vertex1].push(vertex2);
+this.adjacencyList[vertex2].push(vertex1);
+    }
+    removeEdge(vertex1,vertex2){
+this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((v) => v !== vertex2)
+this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((v) => v !== vertex1)   
+}
+removeVertex(vertex){
+while(this.adjacencyList[vertex].length){
+const adjacentVertex = this.adjacencyList[vertex].pop();
+this.removeEdge(vertex,adjacentVertex)
+}
+delete this.adjacencyList[vertex];
+} 
+printAdjacencyList(){
+        for(const vertex in this.adjacencyList){
+            console.log(`${vertex} -> ${this.adjacencyList[vertex].join('')}`)
+        }
+    }
+}
+const g = new Graph();
+g.addVertex('tokyo');
+g.addVertex('japan');
+g.addVertex('india');
+g.addEdge('tokyo','japan')
+g.removeEdge('tokyo','japan')
+g.removeVertex('india')
+g.printAdjacencyList()
